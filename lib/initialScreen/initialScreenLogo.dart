c26939267b9097157885000005cbc67d2cdcd3b7
Bruno T. Farsura <brunofarsura@gmail.com>
2022-09-26 20:24:31 -0300

    feature: create the initial screen with logo

---
+++ lib/initialScreen/initialScreenLogo.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class InitialScreenLogo extends StatefulWidget {
+  const InitialScreenLogo({super.key});
+
+  final Text title = const Text("PowerEffi - Save money and energy!");
+
+  @override
+  State<InitialScreenLogo> createState() => _InitialScreenLogoState();
+}
+
+class _InitialScreenLogoState extends State<InitialScreenLogo> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: widget.title,
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.add_circle),
+              onPressed: () {
+                Navigator.pushNamed(context, "/personalData");
+              })
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Image.asset("images/powerEffiLogo.jpeg",
+          fit: BoxFit.fill),
+      )
+    );
+  }
+}
